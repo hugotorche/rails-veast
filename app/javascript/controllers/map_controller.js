f785c6215868e5b9cc87f0eea402144806907d1c
Hugo Torche <hugo.torche@kedgebs.com>
2025-03-05 00:12:04 +0100

    testing markers

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -60,7 +60,16 @@ export default class extends Controller {
   }
 
   addMarker() {
-    var marker1 = this.L.marker([46.2044, 6.1432]).addTo(this.map)
+    var greenIcon = new this.L.Icon({
+      iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41]
+    });
+
+    var marker1 = this.L.marker([46.2044, 6.1432], {icon: greenIcon}).addTo(this.map)
     .bindPopup('Geneva');
 
     var marker2 = this.L.marker([55.6761, 12.5683]).addTo(this.map)
