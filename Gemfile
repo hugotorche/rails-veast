4f539f403ad80e5d54cb6b1433edcd08bb46dc0c
Hugo Torche <hugo.torche@kedgebs.com>
2025-04-10 23:50:18 +0200

    test city state

--- Gemfile
+++ Gemfile
@@ -19,6 +19,8 @@ gem "bootsnap", require: false
 gem "kamal", require: false
 gem "thruster", require: false
 
+gem 'city-state'
+
 group :development, :test do
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
   gem "brakeman", require: false
--- app/models/map_point.rb
+++ app/models/map_point.rb
@@ -1,3 +1,14 @@
 class MapPoint < ApplicationRecord
     belongs_to :user
+    validates :country, presence: true
+    validates :city, presence: true
+    def countries
+        CS.countries.with_indifferent_access
+    end
+    def cities
+        CS.cities(country) || []
+    end
+    def country_label
+        countries[country]
+    end
 end
